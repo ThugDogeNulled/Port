2f58aed56aae221454507fda5c2d2095851fecd6
ThugDoge <welington_aquino@hotmail.com>
2016-08-01 09:23:22 -0300

    Leblanc II

---
+++ Leblanc II/Program.cs
@@ -0,0 +1,13 @@
+using EloBuddy;
+
+namespace Leblanc
+{
+    internal class Program
+    {
+        
+        private static void Main(string[] args)
+        {
+            Leblanc.Init();
+        }
+    }
+}
